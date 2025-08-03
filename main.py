c8a8bb57ab25c91db6721be157501891d027a4dc
cskadam01 <adam.csokonyi@gmail.com>
2025-08-03 20:15:18 +0200

    Első óra
    Most kezdtük el megnézni a python alapjait
    
    változók, listák, típusok, input függvény

---
+++ main.py
@@ -0,0 +1,60 @@
+gyumolcs = "alma"  #String = szöveg típus
+darab_szam = 5      #Int, Integer = szám típus
+szazalek = 3.45     #Float = maradkos vagy tört szám
+
+a = "5"
+b = 4
+
+kaja1 = "kalács"
+kaja2 = "pörkölt"
+
+atvaltott_szam = int(a) #Típusok átváltása
+str()
+float()                     
+
+
+
+#Több elemet tároló változók
+#list
+gyümölcsök = ["alma", "körte", "barack", "szőlő", "alma"]
+
+#tuple
+zöldségek = ("répa", "retek")
+
+#dict
+
+ország_városok = {
+    "Magyarország" : "Budapest",
+    "Olaszország" : "Róma",
+    "Franciaország" : "Párizs",
+    "Oroszország" : "Moszkva"
+}
+
+felhasználók = {
+    "name":  "xy",
+    "age" : 24,
+    "email":"xy.yx@gmail.com",
+    "pass" : "12345"
+}
+
+felhasz = []
+
+nev = input("Add meg a neved ")
+kor = int(input("Add meg a korod "))
+
+felhasz.append(nev)
+felhasz.append(kor)
+
+felhasz_dict = {
+    "nev" : nev,
+    "kor" : kor
+}
+
+
+
+
+
+
+
+
+
